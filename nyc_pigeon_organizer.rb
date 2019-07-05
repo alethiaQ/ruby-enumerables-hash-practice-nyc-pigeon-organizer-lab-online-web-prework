f11792df5e7d1494fb4bf9043f4ed5a967f65611
Alethia Quintero <alethiaq22@gmail.com>
2019-07-05 22:40:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   sorted_data = {}
   data.each do |attrib, value|
-    value.each do|desc, list|
-    lost.each do|name|
+    value.each do |desc, list|
+    lost.each do |name|
       
       if !sorted_data[name]
         sorted_data[name]= {}
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
           sorted_data[name][attrib]= []
         end
         
-        sorted_data[name][attrib] << 
+        sorted_data[name][attrib] << desc.to_s
         
       end #name loop
     end #desc loop
