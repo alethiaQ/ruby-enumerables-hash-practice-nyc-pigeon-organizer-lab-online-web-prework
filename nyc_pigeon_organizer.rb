d928833728dad14ca27e7acbafcb30f9f5a453b5
Alethia Quintero <alethiaq22@gmail.com>
2019-07-05 22:42:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_data = {}
+  data.each do |attrib, value|
+    value.each do |desc, list|
+    list.each do |name|
+      
+      if !sorted_data[name]
+        sorted_data[name]= {}
+      end
+        if !sorted_data[name][attrib]
+          sorted_data[name][attrib]= []
+        end
+        
+        sorted_data[name][attrib] << desc.to_s
+        
+      end #name loop
+    end #desc loop
+   
+  end #attrib loop
+  sorted_data
 end
