77fe1ba8c7fba749de8897395f8acf054d7551bc
Alethia Quintero <alethiaq22@gmail.com>
2019-07-05 22:37:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,12 +11,9 @@ def nyc_pigeon_organizer(data)
           sorted_data[name][attrib]= []
         end
         
-          sorted_data[name_data] += {
-            attrib => [desc]
-          }
-        end
+        
     
-      end
+     
         
       end #name loop
     end #desc loop
